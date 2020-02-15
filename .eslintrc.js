99e1a8254a543e4891d1fb3416cee7cdd52249bb
Garth Whitaker <garth.whitaker@gmail.com>
2020-02-15 07:11:11 +0000

    Update remote repository with current local changes

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "linebreak-style": ["error", "unix"]
+  }
+};
